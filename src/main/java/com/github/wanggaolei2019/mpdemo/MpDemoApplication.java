044e041d84e38741097b241a86948526077ad2eb
wanggl <wang-gaolei@qq.com>
2021-05-07 16:59:51 +0800

    Initial commit

---
+++ src/main/java/com/github/wanggaolei2019/mpdemo/MpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.wanggaolei2019.mpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpDemoApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MpDemoApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/github/wanggaolei2019/mpdemo/MpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wanggaolei2019.mpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpDemoApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
